28864638f49b304a3b751ba2a4b571d20deffc35
ChloeArd <chloe.ardoise@gmail.com>
2020-12-01 14:17:26 +0100

    Exercice 64

---
+++ app.js
@@ -0,0 +1,21 @@
+let nombre3 = 3;
+let nombre5 = 5;
+
+for (let nombre = 1; nombre <= 100; nombre++) {
+
+    if ((nombre % nombre3) === 0 && (nombre % nombre5) === 0) {
+        console.log(nombre + " => \"fizzbuzz\"");
+    }
+
+    else if ((nombre % nombre3) === 0) {
+        console.log(nombre + " => \"fizz\"");
+    }
+
+    else if ((nombre % nombre5) === 0) {
+        console.log(nombre + " => \"buzz\"");
+    }
+
+    else {
+        console.log(nombre + " => " + nombre);
+    }
+}
